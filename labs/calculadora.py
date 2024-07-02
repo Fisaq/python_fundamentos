3fe66e9d3d1baa7a913805f0eb29095ab5c34f61
Isaque Ferreira <95030412+Fisaq@users.noreply.github.com>
2024-07-02 14:28:38 -0300

    Create calculadora.py

---
+++ labs/calculadora.py
@@ -0,0 +1,57 @@
+# Programa que implementa uma Calculadora
+
+def soma(num1,num2):
+    resultado =  num1 + num2
+    print(f'\n{num1} + {num2} = {resultado}')
+
+def sub(num1,num2):
+    resultado =  num1 - num2
+    print(f'\n{num1} - {num2} = {resultado}')
+
+def div(num1,num2):
+    resultado =  num1 / num2
+    print(f'\n{num1} / {num2} = {resultado}')
+
+def mult(num1,num2):
+    resultado =  num1 * num2
+    print(f'\n{num1} * {num2} = {resultado}')
+
+def potencia(num1,num2):
+    resultado = num1 ** num2
+    print(f'\n{num1} ^ {num2} = {resultado}')
+
+def menu():
+    
+    print('\n-------------------- PYCALC --------------------\n')
+
+    while True:
+
+        print('\nSelecione a operação que deseja realizar:\n')
+        print('[1] Somar\n[2] Subtrair\n[3] Dividir\n[4] Multiplicar\n[5] Potenciação')
+        op = int(input('\nR: '))
+
+        lista_op = [1, 2, 3, 4, 5]
+
+        if op not in lista_op:
+            print('Opção inválida, favor selecionar uma opção do menu!')
+        else:
+            num1 = int(input('\nInforme o primeiro valor: '))
+            num2 = int(input('\nInforme o segundo valor: '))
+            if op == 1:
+                soma(num1,num2)
+                break
+            elif op == 2:
+                sub(num1,num2)
+                break
+            elif op == 3:
+                div(num1,num2)
+                break
+            elif op == 4:
+                mult(num1,num2)
+                break
+            elif op == 5:
+                potencia(num1,num2)
+                break
+
+if __name__ == '__main__':
+    menu()
